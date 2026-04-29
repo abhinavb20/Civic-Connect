363b22e06a0ac41ceafeef5246b9634bb4b70106
Abhinav B <abhinavbahuleyan7@gmail.com>
2026-04-29 20:21:58 +0530

    fixed image path

--- Client/src/Guest/Pages/Dashboard/Dashboard.jsx
+++ Client/src/Guest/Pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import style from "./Dashboard.module.css";
 import { Link } from "react-router-dom";
-import mapImage from "../../Components/assets/Image.png";
+import mapImage from "../../../Guest/Components/assets/Image.png";
 const Dashboard = () => {
 
   const features = [
