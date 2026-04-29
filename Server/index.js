29c5f2bfd56786265a6b304b41ea2a72335c0f41
Abhinav B <abhinavbahuleyan7@gmail.com>
2026-04-29 22:50:56 +0530

    added root route and render port fix

--- Server/index.js
+++ Server/index.js
@@ -10,12 +10,16 @@ const fs = require("fs");
 
 // -------------------- App Setup --------------------
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
 
 // -------------------- Database Connection --------------------
 mongoose
